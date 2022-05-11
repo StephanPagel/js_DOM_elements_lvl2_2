e7a8a0969386953c66adfabf285b7bcef6a563e8
StephanPagel <stephan.pagel84@gmail.com>
2022-05-11 09:22:02 +0200

    finished

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let vorname = document.getElementById('vorname');
+let nachname = document.getElementById('nachname');
+let land = document.getElementById('land');
+
+function results() {
+    console.log("function works");
+    console.log(vorname.value);
+    console.log(nachname.value);
+    console.log(land.value);
+}
